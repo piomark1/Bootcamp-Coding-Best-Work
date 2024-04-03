27eb6a54a8c69c5fac073df79344db20b847a78e
piomark1 <piotrek.markowicz1@gmail.com>
2024-04-03 21:08:42 +0100

    changed code flow, documentation and error handling in alternative.py file

--- alternative/alternative.py
+++ alternative/alternative.py
@@ -1,81 +1,85 @@
-# Write a program that reads in a string and makes each alternate character
-# into an upper case character and each other alternate character a lower case character.
-# and each other alternate character a lower case character.
-#
-# e.g. The string "Hello World" would becom "HeLlO WoRlD"
-#
-# Now, try starting with the same string but making each alternative word
-# lower and upper case.
-#
-# e.g. The string: "I am learning to code" would become "i AM learning TO code".
-#
-# Tip: Using the split() and join() functions will help you here
+'''
+ Write a program that reads in a string and makes each alternate character
+ into an upper case character and each other alternate character a lower case character.
+ and each other alternate character a lower case character.
 
-# 1. Input any string You want, index = 0, new_string = ""
-# 2. While index < len(the_string)(False) go to 4, (True): 
-#
-#    *Take index element of the string, index:{0, 1, 2...index}:   
-#       -if index % 2 == 0(True): new_string = new_string + the_string[index].upper()
-#       -else index % 2 == 0(False): new_string = new_string + the_string[index].lower()
-#
-# 3. index+=1, go to step 2.
-# 4. While index < len(the_string)(False), no loop entry, output new_string, end program.
+ e.g. The string "Hello World" would becom "HeLlO WoRlD"
 
-#============================ALTERNATION EVERY LETTER=======================================
+ Now, try starting with the same string but making each alternative word
+ lower and upper case.
 
-#set up: string input, new_string = empty string, index = 0
-the_string = input("Enter any sentence You wish: ") 
-index = 0
-new_string = ""     #for storing alternated letters                                              
+ e.g. The string: "I am learning to code" would become "i AM learning TO code".
 
-while index < len(the_string):          # index less than the lenght of "the_string" = True, go into the loop, otherwise
-                                        # output "new_string", end program
+ Tip: Using the split() and join() functions will help you here
+'''                                              
+#check is entered string not empty 
+while True:
 
-                         
-    if index % 2 == 0:                                           #if True, transform "the_string[index]" uppercase, add to "new_string", store in "new_string"
-        new_string = new_string + the_string[index].upper()      #"the_string[index] = the_string[index].upper()" generates error
-                                                                 #we need new variable "new_string"        
-    else:
-        new_string = new_string + the_string[index].lower()      #if "index % 2 == 0" is False, tranform "the_string[index]" lowercase,
-                                                                 #add to "new_string", store in "new_string"   
-    
-    index += 1          #index = index + 1            
-
-print(new_string)   #output "new_string" because of no entry to the loop but jump over the loop, end program
-
-#============================ALTERNATION EVERY WORD=============================================
-
-# 1. Input any string You want, transform string into list, index = 0
-# 2. While index < len(the_string)(False): go to 4, (True):  
-#
-#    *Take index element of the list, index:{0, 1, 2...index}:   
-#       -if index % 2 == 0(True): the_string[index] = the_string[index].lower()
-#       -else index % 2 == 0(False): the_string = the_string[index].upper()
-# 
-# 3. index+=1, go to step 2.
-# 4. While index < len(the_string)(False), no loop entry, transform list the_string into string the_string, 
-#     output the_string, end program.
-
-
-#set up: string input, transform the_string into list "the_string", set index = 0
-the_string = input("Enter any sentence You want: ")
-the_string = the_string.split()
-index = 0
-
-while index < len(the_string):    #while index less than the lenght of the list "the_string", go into the loop
-
-    if index%2 == 0:                                        #if "True", transform index element of the list "lowercase"  
-        the_string[index]=the_string[index].lower()         #store in the_string[index]
+    the_string = input("Enter any sentence You want: ")
+        
+    if (the_string == ""):
+                
+        print("Empty string! Try again.")
+        continue
     
     else:
-        the_string[index]=the_string[index].upper()       #else, transform index element of the list "uppercase"  
-                                                          #store in the_string[index]  
-    
-    index += 1                                            #increase index = index + 1, go to begining of the loop  
+        break
 
-the_string = " ".join(the_string)                         #if index < len(the_string)(False), don't entry to the loop  
-                                                          #transform list "the_string" into the string "the_string" 
-                                                          #and store in "the_string"  
-print(the_string)   #output "the_string"              
+#'new_string_list' will allow store every letter of entered string after 'upper', 'lower' transformation
+new_string_list = []
+
+#loop which goes through every letter in the entered string
+for letter_index in range(0, len(the_string)):          
+                                                        
+    
+    if the_string[letter_index].isalpha() == True:  #is alphabetical? if 'True', check result of 'modulo' operation
+        
+        
+        if letter_index % 2 == 0:   #if 'letter_index' modulo 2 is even transform letter to upper form, 
+                                    #if result is odd, transfrom letter to lower form
+            new_string_list.append(the_string[letter_index].upper())     
+                                                                    
+        else:
+        
+            new_string_list.append(the_string[letter_index].lower())   
+                                                                    
+    #is 'space'? if 'True' append 'space' to 'new_string_list',
+    #if 'no space' and 'no letter' replace mark with empty string          
+    elif the_string[letter_index].isspace() == True:
+        
+            new_string_list.append(the_string[letter_index])
+
+    else:
+            new_string_list.append(the_string[letter_index].replace(the_string[letter_index], ""))                     
+
+#when loop went through the string, join all elements of the list,
+#display transformed string
+new_string_list = "".join(new_string_list)
+
+print("\n%s" %(new_string_list))   
+
+#transfrom 'new_string_list' into list of words
+new_string_list = new_string_list.split()
+
+
+#loop through every 'item_index' in the 'new_string_list'
+#if 'item_index' modulo 2 is even transform 'new_string_list[item_index]' to lower form,
+#if result of modulo operation is odd transform 'new_string_list[item_index] to upper form 
+for item_index in range (0, len(new_string_list)):
+
+    if item_index % 2 == 0:                                           
+        
+        new_string_list[item_index] = new_string_list[item_index].lower()        
+    
+    else:
+        new_string_list[item_index] = new_string_list[item_index].upper()         
+                                                            
+    
+#when loop went through the list, join all elements in one string,
+#display transformed string
+new_string_list = " ".join(new_string_list)                           
+                                                          
+                                                            
+print("\n%s\n" %(new_string_list))           
 
 
